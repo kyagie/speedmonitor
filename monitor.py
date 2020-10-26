97d8c0fc3896e094bcbd2253787e0a8d97bb0552
Arthur Kyagulanyi <akyagulanyi@tayariafrica.com>
2020-10-26 08:51:21 +0300

    Writing to csv file

--- monitor.py
+++ monitor.py
@@ -1,10 +1,20 @@
 import speedtest
 import datetime
+import csv
+
 s = speedtest.Speedtest()
-while True:
-    time = datetime.datetime.now().strftime("%H:%M:%S")
-    downspeed = round((round(s.download()) / 1048576), 2)
-    upspeed = round((round(s.upload()) / 1048576), 2)
-    print(f"time: {time}, downspeed: {downspeed} Mb/s, upspeed: {upspeed} Mb/s")
+with open('test.csv', mode='w') as speedcsv:
+    csv_writer = csv.DictWriter(speedcsv, fieldnames=['time', 'downspeed', 'upspeed'])
+    csv_writer.writeheader()
+    while True:
+        time = datetime.datetime.now().strftime("%H:%M:%S")
+        downspeed = round((round(s.download()) / 1048576), 2)
+        upspeed = round((round(s.upload()) / 1048576), 2)
+        csv_writer.writerow({
+            'time': time,
+            'downspeed': downspeed,
+            'upspeed': upspeed
+            })
+        print(f"time: {time}, downspeed: {downspeed} Mb/s, upspeed: {upspeed} Mb/s")
    # print(s.download(), s.upload())
 
